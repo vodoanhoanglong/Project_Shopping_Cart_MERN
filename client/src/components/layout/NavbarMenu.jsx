922eff3733ed1795c020cbf6fd1379e9dc423fb6
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-07-23 22:17:45 +0700

    Finished Navbars

--- client/src/components/layout/NavbarMenu.jsx
+++ client/src/components/layout/NavbarMenu.jsx
@@ -1,40 +1,60 @@
-import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const NavbarMenu = () => {
-  return (
-    <Navbar expand="lg" bg="light" variant="light" className="NavbarMenu">
-      <Navbar.Brand className="font-weight-bolder text-black">
-        HoangLong's Store
-      </Navbar.Brand>
-      <Navbar.Toggle arial-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link
-            className="font-weight-bolder text-black"
-            to="/dashboard"
-            as={Link}
-          >
-            Dashboard
-          </Nav.Link>
-          <Nav.Link
-            className="font-weight-bolder text-black"
-            to="/about"
-            as={Link}
-          >
-            About
-          </Nav.Link>
-        </Nav>
+import "../../css/NavbarMenu.css";
+import Cart from "../../assets/shopping-cart.png";
 
-        <Nav>
-          <Nav.Link className="font-weight-bolder text-black" disabled>
-            Welcome Long
-          </Nav.Link>
-          <Button>Hi</Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+const NavbarMenu = () => {
+  const [toggle, setToggle] = useState("");
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const scrollPos = window.scrollY;
+      if (scrollPos > 10) setToggle("scrolled");
+      else setToggle("");
+    });
+  }, [toggle]);
+
+  return (
+    <>
+      <header className={"main-header " + toggle}>
+        <div className="logo">
+          <h1>
+            <Link to="/">Clothes</Link>
+          </h1>
+        </div>
+        <input type="checkbox" className="menu-btn" id="menu-btn" />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Shop</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+        <div className="nav-cart">
+          <span>0</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="20" />
+          </Link>
+        </div>
+        <label htmlFor="menu-btn" className={"menu-icon " + toggle}>
+          <span className="menu-icon__line"></span>
+        </label>
+      </header>
+
+      <div className="hero">
+        <h1>Hero Text</h1>
+      </div>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          cum dolorum iusto id adipisci ipsa harum nisi fugiat saepe recusandae?
+        </p>
+      </div>
+    </>
   );
 };
 
