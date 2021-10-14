3120091f2ba960683ba4bcde581340b6c53848c6
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-10-14 11:04:33 +0700

    Finished

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://fast-shelf-17416.herokuapp.com"
-    : "some thing";
+    ? "http://localhost:5000"
+    : "https://fast-shelf-17416.herokuapp.com";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "shoppingCart";
