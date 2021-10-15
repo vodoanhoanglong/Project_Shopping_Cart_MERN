bdebbff0e4003fe48b8728f563c899a002471907
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-10-15 22:08:10 +0700

    edit constants

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://yil-shop-vodoanhoanglong.vercel.app"
+    ? "http://localhost:5000"
     : "https://yil-shop-vodoanhoanglong.vercel.app";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "shoppingCart";
