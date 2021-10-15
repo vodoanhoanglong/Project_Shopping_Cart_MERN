064d6305a709347d1a894bebe34c4bfa04304909
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-10-15 20:22:02 +0700

    update client

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000"
-    : "https://fast-shelf-17416.herokuapp.com";
+    : "https://yil-shopping.vercel.app/";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "shoppingCart";
